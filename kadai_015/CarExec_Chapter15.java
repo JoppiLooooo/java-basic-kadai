df28878a88603ee17736254813996e75922d4173
JoppiLooooo <jr19981102@gmail.com>
2025-09-17 22:11:44 +0900

    Add files via upload

--- kadai_015/CarExec_Chapter15.java
+++ kadai_015/CarExec_Chapter15.java
@@ -2,10 +2,10 @@ package kadai.kadai_015;
 
 public class CarExec_Chapter15 {
 	public static void main(String[] args) {
-		int afterGear = 0;
-		Car_Chapter15 car = new Car_Chapter15( 3, 10);
 		
-		car.chageGear(afterGear);
+		Car_Chapter15 car = new Car_Chapter15();
+		
+		car.chageGear(3);
 		car.run();
 	}
 }
