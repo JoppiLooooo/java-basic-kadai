bbb32767532de8eef61f3aa616b6cb79f49e2cde
JoppiLooooo <jr19981102@gmail.com>
2025-09-17 16:19:42 +0900

    Add files via upload

---
+++ kadai_015/CarExec_Chapter15.java
@@ -0,0 +1,11 @@
+package kadai.kadai_015;
+
+public class CarExec_Chapter15 {
+	public static void main(String[] args) {
+		int afterGear = 0;
+		Car_Chapter15 car = new Car_Chapter15( 3, 10);
+		
+		car.chageGear(afterGear);
+		car.run();
+	}
+}
---
+++ kadai_015/Car_Chapter15.java
@@ -0,0 +1,26 @@
+package kadai.kadai_015;
+
+public class Car_Chapter15 {
+	private int gear  = 1;
+	private int speed = 10;
+	
+	public Car_Chapter15( int gear, int speed) {
+		this.gear = gear;
+		this.speed = speed;
+	}
+	
+	public void chageGear(int afterGear) {
+		switch(this.gear) {
+			case 1 -> this.speed = 10;
+			case 2 -> this.speed = 20;
+			case 3 -> this.speed = 30;
+			case 4 -> this.speed = 40;
+			case 5 -> this.speed = 50;
+			default  -> this.speed = 10;
+		};
+	}
+	
+	public void run() {
+		System.out.println("speed is " + this.speed);
+	}
+}
