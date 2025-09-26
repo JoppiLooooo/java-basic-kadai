7d4805691db4d092e22f88b2e907e633c7a19b9d
JoppiLooooo <jr19981102@gmail.com>
2025-09-26 16:50:48 +0900

    Add files via upload

--- kadai_021/DictionaryExec_Chapter21.java
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -1,14 +1,13 @@
 package kadai.kadai_021;
-import java.util.HashMap;
 
 public class DictionaryExec_Chapter21 {
 	public static void main(String[] args) {
 		
-		HashMap<String,String> dictionaryMap = new HashMap<String,String>();
+		Dictionary_Chapter21 dictionary = new Dictionary_Chapter21();
 	
 		String[] searchWords = {"apple","banana","grape","orange"};
 		
-		dictionaryMap.search(searchWords);
+		dictionary.search(searchWords);
 		
 	}
 }
--- kadai_021/Dictionary_Chapter21.java
+++ kadai_021/Dictionary_Chapter21.java
@@ -4,20 +4,21 @@ import java.util.HashMap;
 
 public class Dictionary_Chapter21 {
 	
-	HashMap<String, String> dictionary = new HashMap<String, String>();
-	
-	dictionary.put("apple","りんご");
-	dictionary.put("peach","桃");
-	dictionary.put("banana","バナナ");
-	dictionary.put("lemon","レモン");
-	dictionary.put("pear","梨");
-	dictionary.put("kiwi","キウィ");
-	dictionary.put("strawberry","いちご");
-	dictionary.put("grape","ぶどう");
-	dictionary.put("muscat","マスカット");
-	dictionary.put("cherry","さくらんぼ");
-	
-	public void search(String searchWords[]) {
+	public void search(String[] searchWords) {
+		
+		HashMap<String, String> dictionary = new HashMap<String, String>();
+		
+		dictionary.put("apple","りんご");
+		dictionary.put("peach","桃");
+		dictionary.put("banana","バナナ");
+		dictionary.put("lemon","レモン");
+		dictionary.put("pear","梨");
+		dictionary.put("kiwi","キウィ");
+		dictionary.put("strawberry","いちご");
+		dictionary.put("grape","ぶどう");
+		dictionary.put("muscat","マスカット");
+		dictionary.put("cherry","さくらんぼ");
+		
 		for( int i = 0; i < searchWords.length; i++) {
 				if(dictionary.get(searchWords[i]) == null) {
 					
